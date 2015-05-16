44fb8d658c24be5cf5ea8e480dfb338c903f06cf
Sandeep Deb <deb.sandeep@gmail.com>
2015-05-16 17:30:28 +0530

    Markdown integration

---
+++ src/test/java/com/sandy/jovenotes/processor/junit/poc/Markdown4JTest.java
@@ -0,0 +1,20 @@
+package com.sandy.jovenotes.processor.junit.poc;
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.Logger;
+
+import com.sandy.jovenotes.processor.util.Markdown;
+
+public class Markdown4JTest extends TestCase {
+	
+	private static final Logger logger = Logger.getLogger( Markdown4JTest.class ) ;
+
+	public void testMD4JSimple() throws Exception {
+		
+		Markdown processor = new Markdown() ;
+		String out = processor.process( "This is  \n**a** Test." ) ;
+		
+		logger.debug( out ) ;
+	}
+}
