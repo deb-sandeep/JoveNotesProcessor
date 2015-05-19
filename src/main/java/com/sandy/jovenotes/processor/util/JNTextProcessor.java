2779df366c117a9d63647b5e49d1274df5022ec6
Sandeep Deb <deb.sandeep@gmail.com>
2015-05-19 10:30:43 +0530

    WIP

--- src/main/java/com/sandy/jovenotes/processor/util/JNTextProcessor.java
+++ src/main/java/com/sandy/jovenotes/processor/util/JNTextProcessor.java
@@ -14,7 +14,7 @@ import com.sandy.jovenotes.processor.core.notes.Chapter;
 
 public class JNTextProcessor {
 
-	private static final Logger log = Logger.getLogger( JNTextProcessor.class ) ;
+	private static final Logger log = Logger.getLogger(JNTextProcessor.class) ;
 	
 	private static PegDownProcessor pdProcessor   = 
 			    new PegDownProcessor( Extensions.ALL & ~Extensions.HARDWRAPS ) ;
